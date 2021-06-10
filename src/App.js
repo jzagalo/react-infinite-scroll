7836c9e655564153902806d935786003760ceb96
Paul Ezaga <paulezaga@gmail.com>
2021-06-10 06:22:50 +0200

    Implementation of AutoScroll

--- src/App.js
+++ src/App.js
@@ -1,51 +1,71 @@
-import React, { Component } from 'react'
+import React, {  useState, useEffect, useRef } from 'react'
 import'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 
 
-class App extends Component {
+function App(){
 
-  constructor(props){
-    super(props);
-    this.state = {
-      pictureGallery: []
-    };
-  }
+  const [ pictureGallery, setPictureGallery ] = useState([]);
+  const [ pageNumber, setPageNumber ] = useState(1);
+  const [ loading, setLoading ] =  useState(false);
+  const numberPerPage = 30;  
   
 
-  async componentDidMount(){
-       await fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bebb1fe4865c7333863f2d73b094566d&tags=cats&per_page=10&format=json&nojsoncallback=1')
-      .then(res => res.json())
-      .then(result  => {
+  const fetchPhotos = async(pageNumber) => {       
+       await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bebb1fe4865c7333863f2d73b094566d&tags=cats&per_page=${numberPerPage}&page=${pageNumber}&format=json&nojsoncallback=1`)
+          .then(res => res.json())
+          .then(result => {
+
             const { photo } = result.photos;
             const pixCollection = photo.map((pix, index) => { 
                 const url = 'https://farm'+ pix.farm + '.staticflickr.com/' + pix.server + '/' + pix.id + '_' + pix.secret + '.jpg'          
                 return(
-                    <div className="hvrbox"  key={index}>
+                    <div className="hvrbox"  key={pix.id + index }>
                         <img src={url} className="App-logo" alt="logo" /> 
                         <div className="hvrbox-layer_top">
                           <div className="hvrbox-text">Picture {index} </div>
                         </div>
-                    </div>  
+                    </div> 
                   )
-            });              
-            
-            this.setState({
-                pictureGallery: pixCollection
-            });             
-        });   
+            });  
 
-      
-        
+            setPictureGallery(pictureGallery => [...pictureGallery, ...pixCollection]);   
+            setLoading(true);          
+        });          
   }
 
-  render(){   
-    return (
-      <div className="container">        
-          { this.state.pictureGallery } 
-      </div>
-    );
+  useEffect(() => {
+    fetchPhotos(pageNumber);
+  }, [pageNumber])
+
+  const pageEnd = useRef();
+
+  const loadMore = () => {
+    setPageNumber(pageNumber => pageNumber + 1);
   }
+
+  useEffect(() => {
+    if(loading){
+        const observer = new IntersectionObserver(entries => {
+          if(entries[0].isIntersecting){
+            loadMore();
+          }
+        },{ threshold: 1});
+        observer.observe(pageEnd.current);
+    }
+  }, [loading]);
+
+   
+  return (
+    <div className="container">        
+        { pictureGallery } 
+      <div> 
+        <h3>{ pictureGallery.length }</h3>    
+        <button onClick={loadMore} ref={pageEnd}>Load More</button>  
+      </div>   
+    </div>
+  );
+ 
 }
 
 export default App;
