031b7a93e05a9f4dae9d97b23e070fc18b047e94
Paul Ezaga <paulezaga@gmail.com>
2021-06-11 14:52:36 +0200

    Set Favourites Feature Added to Photos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 import React, {  useState, useEffect, useRef } from 'react'
 import'bootstrap/dist/css/bootstrap.css'
 import './App.css'
-
+import mainLoader from'./loader.gif';
 
 function App(){
 
   const [ pictureGallery, setPictureGallery ] = useState([]);
   const [ pageNumber, setPageNumber ] = useState(1);
-  const [ loading, setLoading ] =  useState(false);
-  const numberPerPage = 30;  
-  
+  const [ loading, setLoading ] =  useState(false); 
+  const numberPerPage = 10;  
+  const myRefs= useRef([])
 
-  const fetchPhotos = async(pageNumber) => {       
+  const getFavorite = (id) => {
+    return localStorage.getItem(id) === 'true';
+  };
+
+  const setFavorite = (id) => {          
+      localStorage.setItem(id, true);
+  };
+
+  const toggleFavorite = (pix) => {     
+    getFavorite(pix.id) === true ?  localStorage.setItem(pix.id, false): setFavorite(pix.id);
+    myRefs.current[pix.id].setAttribute("id", getFavorite(pix.id))    
+  }
+
+  async function fetchPhotos(pageNumber) {           
        await fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bebb1fe4865c7333863f2d73b094566d&tags=cats&per_page=${numberPerPage}&page=${pageNumber}&format=json&nojsoncallback=1`)
           .then(res => res.json())
           .then(result => {
 
             const { photo } = result.photos;
             const pixCollection = photo.map((pix, index) => { 
+                
+                let favorite = getFavorite(pix.id);
+                if(favorite === undefined){
+                  favorite = false;
+                  setFavorite(pix.id, favorite);                  
+                } 
+                const favoriteClass = favorite.toString();
+              
+
                 const url = 'https://farm'+ pix.farm + '.staticflickr.com/' + pix.server + '/' + pix.id + '_' + pix.secret + '.jpg'          
                 return(
-                    <div className="hvrbox"  key={pix.id + index }>
+                    <div className="hvrbox"  key={pix.id + index }>                         
                         <img src={url} className="App-logo" alt="logo" /> 
-                        <div className="hvrbox-layer_top">
-                          <div className="hvrbox-text">Picture {index} </div>
+                        <div className="hvrbox-layer_top">                           
+                          <div className="hvrbox-text">
+                          <h5 className="underline">Picture </h5>  
+                          <div>{pix.id}</div> 
+                          <button onClick={ () => toggleFavorite(pix) }   ref={(el) => {myRefs.current[pix.id]=el }}  id={favoriteClass}>
+                              Toggle Favorite
+                              <i class="fav-icon">&hearts;</i> 
+                           </button> 
                         </div>
                     </div> 
+                    </div>
                   )
             });  
 
@@ -34,36 +63,44 @@ function App(){
         });          
   }
 
-  useEffect(() => {
-    fetchPhotos(pageNumber);
+  useEffect(() => {    
+    fetchPhotos(pageNumber);        
   }, [pageNumber])
 
   const pageEnd = useRef();
 
   const loadMore = () => {
+    localStorage.setItem("id", 1);
     setPageNumber(pageNumber => pageNumber + 1);
+    console.log(localStorage.getItem('id'));
   }
 
+  
+
   useEffect(() => {
-    if(loading){
-        const observer = new IntersectionObserver(entries => {
-          if(entries[0].isIntersecting){
+    if(loading) {
+        const observer = new IntersectionObserver(entries => {          
+          if(entries[0].isIntersecting){            
             loadMore();
           }
-        },{ threshold: 1});
+        },{ threshold: .9});
         observer.observe(pageEnd.current);
     }
   }, [loading]);
 
    
   return (
-    <div className="container">        
-        { pictureGallery } 
-      <div> 
-        <h3>{ pictureGallery.length }</h3>    
-        <button onClick={loadMore} ref={pageEnd}>Load More</button>  
-      </div>   
-    </div>
+    <div>
+        <div className="container">        
+          { pictureGallery } 
+        </div> 
+        <div className="loader">
+          <img src={mainLoader}  alt="loader" height="40" />  
+        </div>    
+        <button onClick={loadMore} ref={pageEnd} className="load-more">
+          Load More { pictureGallery.length }
+        </button>         
+    </div>    
   );
  
 }
